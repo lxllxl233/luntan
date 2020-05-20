a6fe720dd9fa74e0f9246d30e6407cf78553c05a
lxllxl233 <2209706071@qq.com>
2020-05-20 20:41:32 +0800

    提交

---
+++ src/test/java/com/woqiyounai/luntan/LuntanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woqiyounai.luntan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuntanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
